e156b3b789e14757ca4100c54bd423222d4ff874
joe <joseph.gakure@thejitu.com>
2023-05-04 16:56:58 +0300

    contactus form

--- cypress/utilis/data.js
+++ cypress/utilis/data.js
@@ -2,6 +2,37 @@ const links = [
     "","team", "investments", "contact"
  ]
 
-module.exports ={
-    links
+ 
+ const contact =[
+     {key: "#wpforms-5113-field_0", value: "joe"},
+     {key: "#wpforms-5113-field_0-last", value: "doe"},
+     {key: "#wpforms-5113-field_1", value: "test@gmail.com"},
+     {key: "#wpforms-5113-field_2", value: "test"},
+]
+
+const updateArray = (arr, searchName, newValue)=>{
+
+    let cloneArray = [...arr]
+
+    const objectKey = cloneArray.findIndex((obj)=> obj.key === searchName)
+
+    const updateObj = {...cloneArray[objectKey], value: newValue}
+
+    const updateArray = [
+        ...cloneArray.slice(0, objectKey), updateObj, ...cloneArray.slice(objectKey + 1)
+    ]
+
+    cloneArray = updateArray
+
+    const name = cloneArray
+
+    return name
 }
+    
+    module.exports ={
+        links,
+        contact,
+        updateArray
+    }
+
+
---
+++ cypress/utilis/test.js
@@ -0,0 +1,13 @@
+const contact =[
+    {key: "#wpforms-5113-field_0", value: "joe"},
+    {key: "#wpforms-5113-field_0-last", value: "doe"},
+    {key: "#wpforms-5113-field_1", value: "test@gmail.com"},
+    {key: "#wpforms-5113-field_2", value: "test"},
+]
+   
+console.log(contact);
+
+
+let fruits = ["mango", "apple", "banana", "pineapple"]
+let b= fruits.slice(0,4)
+console.log(b);
